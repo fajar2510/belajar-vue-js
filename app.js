aa3b54a923c447c72591a44f6919dea26b41b826
fajar2510 <sepertiseharusnyafajar@gmail.com>
2022-09-16 08:59:02 +0700

    interactive-click-events

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const app = Vue.createApp({
-    //data, function
-    template: '<h2> I am the template</h2>'
+    data() {
+        return {
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: 45
+        }
+    },
+    methods: {
+        changeTitle(title) {
+            this.title = title
+        //    this.title = 'Words of Radiance'
+        }
+    }
 })
 
 app.mount('#app')
